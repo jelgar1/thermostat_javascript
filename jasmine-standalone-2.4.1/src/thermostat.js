d4e400a61bd81ad23248280b01ed1a3a31f11547
tmgree <tmgree@outlook.com>
2016-01-06 14:49:32 +0000

    remastered

--- jasmine-standalone-2.4.1/src/thermostat.js
+++ jasmine-standalone-2.4.1/src/thermostat.js
@@ -5,13 +5,12 @@ this.power_save = false;
 var min_temp = 10;
 
 Thermostat.prototype.up = function() {
-  if(this.power_save === true) {if (this.temperature === 25) {throw("Temperature cannot go above 25 when power saving is on");}
-  else {this.temperature += 1; return this.temperature;};}
-  else {if (this.temperature === 32) {throw("Temperature cannot go above 32 when power save is off");}
-  else {this.temperature += 1; return this.temperature;};}
+  if(this.power_save === true && this.temperature === 25) {throw("Temperature cannot go above 25 when power saving is on");}
+  else if(this.power_save === false && this.temperature === 32) {throw("Temperature cannot go above 32 when power save is off");}
+  else {this.temperature += 1; return this.temperature;}
 };
 
 Thermostat.prototype.down = function() {
   if (this.temperature === min_temp) {throw("Temperature cannot go under 10");}
-  else {this.temperature -= 1; return this.temperature;};
+  else {this.temperature -= 1; return this.temperature;}
 };
